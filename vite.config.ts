b96d4332b6d543f0e8726e676bc4433a067cb8ca
Yevhenii Pankratov <pankratov.yevhenii999@gmail.com>
2024-10-01 14:06:07 +0300

    added preload plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+// import preloadImagesPlugin from "./vite-plugin-preload-images.ts";
+// import { createHtmlPlugin } from "vite-plugin-html";
+import react from "@vitejs/plugin-react";
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    UnpluginInjectPreload({
+      injectTo: 'head',
+      files: [
+        {
+          entryMatch: /.*\.(png|svg)$/,
+          outputMatch: /.*\.(png|svg)$/,
+        },
+      ],
+    }),
+  ],
+});
